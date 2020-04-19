230fe726c1f4fab700d43a0bff26a3eb66d95de7
xHacker404 <sulaiman.haque@gmail.com>
2020-04-19 19:12:10 +0600

    pom file updated

--- src/main/java/com/shifan/covid19/controllers/HomeController.java
+++ src/main/java/com/shifan/covid19/controllers/HomeController.java
@@ -5,12 +5,17 @@
  */
 package com.shifan.covid19.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  *
  * @author dimiksonkha
  */
+@Controller
 public class HomeController {
     
+    @RequestMapping("/")
     public String showMessage() {
 		return "index";
 	}

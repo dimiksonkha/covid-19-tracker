1229dad1fdcb4f638580f34cefb5f960a79c24fb
xHacker404 <sulaiman.haque@gmail.com>
2020-04-19 19:35:27 +0600

    home controller and CovidStatus model updated

--- src/main/java/com/shifan/covid19/controllers/HomeController.java
+++ src/main/java/com/shifan/covid19/controllers/HomeController.java
@@ -5,7 +5,11 @@
  */
 package com.shifan.covid19.controllers;
 
+import com.shifan.covid19.models.CovidStatus;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -15,9 +19,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
     
+    private List<CovidStatus> covidStatues = new ArrayList<CovidStatus>();
+    CovidStatus covSta1= new CovidStatus("Bangladesh", 2445, 12.17, 75, 91);
+    
     @RequestMapping("/")
-    public String showMessage() {
-		return "index";
+    public String showMessage(Model model) {
+	model.addAttribute("status",covSta1);
+        return "index";
+               
 	}
     
 }
